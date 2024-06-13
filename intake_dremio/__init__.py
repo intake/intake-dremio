524e0077610fc98f9ce4a50edb6e55ebf977e46e
Philipp Rudiger <prudiger@anaconda.com>
2024-06-13 10:31:06 +0200

    Update versioneer for python 3.12 support

--- intake_dremio/__init__.py
+++ intake_dremio/__init__.py
@@ -1,10 +1,7 @@
-from ._version import get_versions
-
-__version__ = get_versions()['version']
-
-del get_versions
-
 import intake # noqa
 
 from .intake_dremio import DremioSource # noqa
 from .dremio_cat import DremioCatalog # noqa
+
+from . import _version
+__version__ = _version.get_versions()['version']
