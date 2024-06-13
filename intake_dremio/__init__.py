90855e6cfc35beccf9d2e39b014fce0667427ba1
Philipp Rudiger <prudiger@anaconda.com>
2024-06-13 10:35:20 +0200

    Fix import order

--- intake_dremio/__init__.py
+++ intake_dremio/__init__.py
@@ -1,7 +1,7 @@
+from . import _version
+__version__ = _version.get_versions()['version']
+
 import intake # noqa
 
 from .intake_dremio import DremioSource # noqa
 from .dremio_cat import DremioCatalog # noqa
-
-from . import _version
-__version__ = _version.get_versions()['version']
